9f7db23e4135ab45c4c47835f82ab19557be8b26
Alice Yang <yangalice2008@gmail.com>
2014-02-03 16:29:53 -0500

    Implemented initialization for LSystemNode

--- HW2/MayaPluginHw2/LSystemNode.cpp
+++ HW2/MayaPluginHw2/LSystemNode.cpp
@@ -1,3 +1,7 @@
+#define MNoVersionString
+#define MNoPluginEntry
+
+#include <maya/MFnPlugin.h>
 #include <maya/MTime.h>
 #include <maya/MFnMesh.h>
 #include <maya/MPoint.h>
@@ -5,9 +9,10 @@
 #include <maya/MFloatPointArray.h>
 #include <maya/MIntArray.h>
 #include <maya/MDoubleArray.h>
+
 #include <maya/MFnUnitAttribute.h>
 #include <maya/MFnTypedAttribute.h>
-#include <maya/MFnPlugin.h>
+#include <maya/MFnNumericAttribute.h>
 
 #include <maya/MPxNode.h>
 #include <maya/MObject.h>
@@ -19,16 +24,15 @@
 
 MStatus returnStatus;
 
+// Igor's preprocessors: To use, simply do something like
+// time = unitAttr.create( "time", "tm", MFnUnitAttribute::kTime, 0.0, &returnStatus );
+// McheckErr(returnStatus, "ERROR creating animCube time attribute\n");
+// MAKE_INPUT(unitAttr);
 #define McheckErr(stat,msg)			\
 	if ( MS::kSuccess != stat ) {	\
 		cerr << msg;				\
 		return MS::kFailure;		\
 	}
-
-// Igor's preprocessors: To use, simply do something like
-// time = unitAttr.create( "time", "tm", MFnUnitAttribute::kTime, 0.0, &returnStatus );
-// McheckErr(returnStatus, "ERROR creating animCube time attribute\n");
-// MAKE_INPUT(unitAttr);
 
 #define MAKE_INPUT(attr) \
 CHECK_MSTATUS(attr.setKeyable(true)); \
@@ -57,11 +61,100 @@ public:
 	virtual MStatus compute(const MPlug& plug, MDataBlock& data);
 	static  void*	creator();
 	static  MStatus initialize();
+	
+public:
+	static MTypeId id;
+	static MObject time;
+	static MObject angle;
+	static MObject stepSize;
+	static MObject grammarFile;
+	static MObject outputMesh;
 
-	static MObject	time;
-	static MObject	outputMesh;
-	static MTypeId	id;
-
-protected:
-	MObject createMesh(const MTime& time, MObject& outData, MStatus& stat);
+//protected:
+//	MObject createMesh(const MTime& time, MObject& outData, MStatus& stat);
 };
+
+// This initializes the node’s identifier to a unique tag
+MTypeId LSystemNode::id (0x0); 
+
+//The attributes of the node are initialized to NULL values. 
+MObject LSystemNode::time;
+MObject LSystemNode::angle;
+MObject LSystemNode::stepSize;
+MObject LSystemNode::grammarFile;
+MObject LSystemNode::outputMesh;
+
+// The creator() method simply returns new instances of this node. The return type is a void* so
+// Maya can create node instances internally in a general fashion without having to know the return type.
+void* LSystemNode::creator()
+{
+	return new LSystemNode;
+}
+
+// The initialize method is called only once when the node is first registered with Maya. In this method 
+// you define the attributes of the node, what data comes in and goes out of the node that other nodes may want to connect to. 
+MStatus LSystemNode::initialize()
+{
+	MFnNumericAttribute numAttr;
+	MFnTypedAttribute typedAttr;
+	MFnUnitAttribute unitAttr;
+
+	MStatus returnStatus;
+
+	// Create attributes
+	LSystemNode::time = unitAttr.create( "time", "tm", MFnUnitAttribute::kTime, 0.0, &returnStatus );
+	McheckErr(returnStatus, "ERROR creating LSystemNode time attribute\n");
+	MAKE_INPUT(unitAttr);
+
+	LSystemNode::angle = numAttr.create( "angle", "a", MFnNumericData::kDouble, 0.0, &returnStatus );
+	McheckErr(returnStatus, "ERROR creating LSystemNode angle attribute\n");
+	MAKE_INPUT(numAttr);
+
+	LSystemNode::stepSize = numAttr.create( "stepSize", "ss", MFnNumericData::kDouble, 0.0, &returnStatus );
+	McheckErr(returnStatus, "ERROR creating LSystemNode stepSize attribute\n");
+	MAKE_INPUT(numAttr);
+
+	LSystemNode::grammarFile = typedAttr.create( "grammarFile", "g", MFnData::kString, &returnStatus );
+	McheckErr(returnStatus, "ERROR creating LSystemNode grammarFile attribute\n");
+	MAKE_INPUT(typedAttr);
+
+	LSystemNode::outputMesh = typedAttr.create( "outputMesh", "out", MFnData::kMesh, &returnStatus );
+	McheckErr(returnStatus, "ERROR creating LSystemNode outputMesh attribute\n");
+	MAKE_OUTPUT(typedAttr);
+
+	// Add attributes
+	returnStatus = addAttribute(LSystemNode::time);
+	McheckErr(returnStatus, "ERROR adding time attribute\n");
+
+	returnStatus = addAttribute(LSystemNode::angle);
+	McheckErr(returnStatus, "ERROR adding angle attribute\n");
+
+	returnStatus = addAttribute(LSystemNode::stepSize);
+	McheckErr(returnStatus, "ERROR adding stepSize attribute\n");
+
+	returnStatus = addAttribute(LSystemNode::grammarFile);
+	McheckErr(returnStatus, "ERROR adding grammarFile attribute\n");
+
+	returnStatus = addAttribute(LSystemNode::outputMesh);
+	McheckErr(returnStatus, "ERROR adding outputMesh attribute\n");
+
+	// Connect inputs to outputs via attributeAffects
+	returnStatus = attributeAffects(LSystemNode::time, LSystemNode::outputMesh);
+	McheckErr(returnStatus, "ERROR in attributeAffects\n");
+	
+	returnStatus = attributeAffects(LSystemNode::angle, LSystemNode::outputMesh);
+	McheckErr(returnStatus, "ERROR in attributeAffects\n");
+
+	returnStatus = attributeAffects(LSystemNode::stepSize, LSystemNode::outputMesh);
+	McheckErr(returnStatus, "ERROR in attributeAffects\n");
+
+	returnStatus = attributeAffects(LSystemNode::grammarFile, LSystemNode::outputMesh);
+	McheckErr(returnStatus, "ERROR in attributeAffects\n");
+
+	return MS::kSuccess;
+}
+
+MStatus LSystemNode::compute(const MPlug& plug, MDataBlock& data)
+{
+	return MS::kSuccess;
+}
