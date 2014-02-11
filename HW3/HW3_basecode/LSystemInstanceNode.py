629fd444a6ec8e81881147621622021929ca7b14
Alice Yang <yangalice2008@gmail.com>
2014-02-10 20:52:21 -0500

    Have a rudimentary tree going :)

--- HW3/HW3_basecode/LSystemInstanceNode.py
+++ HW3/HW3_basecode/LSystemInstanceNode.py
@@ -52,53 +52,86 @@ class LSystemInstanceNode(OpenMayaMPx.MPxNode):
     # compute
     def compute(self,plug,data):
         # IN PROGRESS:: create the main functionality of the node.
-        if plug == LSystemInstanceNode.outputBranches or plug == LSystemInstanceNode.outputFlowers:
+        #if plug == LSystemInstanceNode.outputBranches or plug == LSystemInstanceNode.outputFlowers:
         
-            print "Compute!\n"
+        print "Compute!\n"
+        
+        # Retrieve input values from their data handles
+        angleData = data.inputValue(LSystemInstanceNode.angle)
+        angleValue = angleData.asDouble()
+        print "angleValue = %d" % (angleValue)
+        
+        stepSizeData = data.inputValue(LSystemInstanceNode.stepSize)
+        stepSizeValue = stepSizeData.asDouble()
+        print "stepSizeValue = %d" % (stepSizeValue)
+        
+        grammarFileData = data.inputValue(LSystemInstanceNode.grammarFile)
+        grammarFileValue = grammarFileData.asString()
+        print "grammarFileValue = %s" % (grammarFileValue)
+        
+        iterationsData = data.inputValue(LSystemInstanceNode.iterations)
+        iterationsValue = iterationsData.asDouble()
+        print "iterationsValue = %d \n" % (iterationsValue)
+        
+        # Output values
+        outBranchesData = data.outputValue(LSystemInstanceNode.outputBranches) #the MDataHandle
+        outBranchesAAD = OpenMaya.MFnArrayAttrsData() #the MFnArrayAttrsData
+        outBranchesObject = outBranchesAAD.create() #the MObject
+        
+        outFlowersData = data.outputValue(LSystemInstanceNode.outputFlowers) #the MDataHandle
+        outFlowersAAD = OpenMaya.MFnArrayAttrsData() #the MFnArrayAttrsData
+        outFlowersObject = outFlowersAAD.create() #the MObject
+        
+        # Create the vectors for position, id, scale, and aimDirection (branch end - start) for Branches
+        positionArrayBranch = outBranchesAAD.vectorArray("position")
+        idArrayBranch = outBranchesAAD.doubleArray("id")
+        scaleArrayBranch = outBranchesAAD.vectorArray("scale")
+        aimDirArrayBranch = outBranchesAAD.vectorArray("aimDirection")
+        
+        # Create the vectors for position, id, scale, and aimDirection (branch end - start) for Flowers
+        positionArrayFlower = outFlowersAAD.vectorArray("position")
+        idArrayFlower = outFlowersAAD.doubleArray("id")
+        scaleArrayFlower = outFlowersAAD.vectorArray("scale")
+        aimDirArrayFlower = outFlowersAAD.vectorArray("aimDirection")
+        
+        # TODO:: Run the LSystem and set the positions/ids/ etc
+        system = LSystem.LSystem()
+        system.loadProgram(str(grammarFileValue))
+        system.setDefaultAngle(angleValue)
+        system.setDefaultStep(stepSizeValue)
+        
+        branches = LSystem.VectorPyBranch()
+        flowers = LSystem.VectorPyBranch()
+                
+        for i in range (0, int(iterationsValue)):
+            insn = system.getIteration(i)
+            print insn
+            system.processPy(i, branches, flowers)
             
-            # Create a new LSystem object
-            system = LSystem.LSystem()
+        # Loop through branches to fill the arrays     
+        for idx,branch in enumerate(branches):
+            print "===== BRANCH START ===="
+            print "idx: " + str(idx)
+            print "branch: "
+            print branch 
+            startPos = OpenMaya.MVector(branch[0], branch[1], branch[2])
+            endPos = OpenMaya.MVector(branch[3], branch[4], branch[5])
+            dir = endPos - startPos 
             
-            # Retrieve input values from their data handles
-            angleData = data.inputValue(LSystemInstanceNode.angle)
-            angleValue = angleData.asDouble()
-            
-            stepSizeData = data.inputValue(LSystemInstanceNode.stepSize)
-            stepSizeValue = stepSizeData.asDouble()
-            
-            grammarFileData = data.inputValue(LSystemInstanceNode.grammarFile)
-            grammarFileValue = grammarFileData.asString()
-            
-            iterationsData = data.inputValue(LSystemInstanceNode.iterations)
-            iterationsValue = iterationsData.asDouble()
-            
-            # Output values
-            outBranchesData = data.outputValue(LSystemInstanceNode.outputBranches) #the MDataHandle
-            outBranchesAAD = OpenMaya.MFnArrayAttrsData() #the MFnArrayAttrsData
-            outBranchesObject = outBranchesAAD.create() #the MObject
-            
-            outFlowersData = data.outputValue(LSystemInstanceNode.outputFlowers) #the MDataHandle
-            outFlowersAAD = OpenMaya.MFnArrayAttrsData() #the MFnArrayAttrsData
-            outFlowersObject = outFlowersAAD.create() #the MObject
-            
-            # Create the vectors for position, id, scale, and aimDirection (branch end - start) for Branches
-            positionArrayBranch = outBranchesAAD.vectorArray("position")
-            idArrayBranch = outBranchesAAD.doubleArray("id")
-            scaleArrayBranch = outBranchesAAD.vectorArray("scale")
-            aimDirArrayBranch = outBranchesAAD.vectorArray("aimDirection")
-            
-            # Create the vectors for position, id, scale, and aimDirection (branch end - start) for Flowers
-            positionArrayFlower = outFlowersAAD.vectorArray("position")
-            idArrayFlower = outFlowersAAD.doubleArray("id")
-            scaleArrayFlower = outFlowersAAD.vectorArray("scale")
-            aimDirArrayFlower = outFlowersAAD.vectorArray("aimDirection")
-            
-            # TODO:: Run the LSystem and set the positions/ids/ etc
-            ############################################################
-            
-            # Finally set the output data handles
-            outBranchesData.setMObject(outBranchesObject)
-            outFlowersData.setMObject(outFlowersObject)
+            positionArrayBranch.append(startPos)
+            idArrayBranch.append(idx)
+            scaleArrayBranch.append(OpenMaya.MVector(2,2,2))
+            aimDirArrayBranch.append(dir)
+            print "======== BRANCH END ========\n\n"
+                    
+        
+        # Loop through flowers to fill the arrays
+        for flower in flowers:
+            print flower      
+        
+        # Finally set the output data handles
+        outBranchesData.setMObject(outBranchesObject)
+        outFlowersData.setMObject(outFlowersObject)
 
         data.setClean(plug)
 
@@ -124,10 +157,10 @@ def nodeInitializer():
     MAKE_INPUT(nAttr)
         
     # Output attributes
-    LSystemInstanceNode.outputBranches = tAttr.create("outBranches", "ob", OpenMaya.MFnArrayAttrsData.kDynArrayAttrs)
+    LSystemInstanceNode.outputBranches = tAttr.create("outputBranches", "ob", OpenMaya.MFnArrayAttrsData.kDynArrayAttrs)
     MAKE_OUTPUT(tAttr)
     
-    LSystemInstanceNode.outputFlowers = tAttr.create("outFlowers", "of", OpenMaya.MFnArrayAttrsData.kDynArrayAttrs)
+    LSystemInstanceNode.outputFlowers = tAttr.create("outputFlowers", "of", OpenMaya.MFnArrayAttrsData.kDynArrayAttrs)
     MAKE_OUTPUT(tAttr)
     
     try:
